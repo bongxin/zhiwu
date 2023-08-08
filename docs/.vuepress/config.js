b693762fa5fa88eb9f41c72b552c0085d3ee3034
xiekx <xiekx@huaweisoft.com>
2023-08-08 17:29:36 +0800

    支持主题切换

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,22 @@
-import { defaultTheme } from "vuepress";
-import theme from "./theme.js";
-import { searchPlugin } from '@vuepress/plugin-search'
+import { defaultTheme } from 'vuepress';
+import theme from './theme.js';
+import { searchPlugin } from '@vuepress/plugin-search';
 
 export default {
-    base: '/zhiwu',
-    lang: 'zh-CN',
-    title: '知屋',
-    description: '一个房屋管理系统',
-    theme: theme,
-    plugins: [
-        searchPlugin({
-            // 配置项
-        }),
-    ],
+  base: '/zhiwu',
+  lang: 'zh-CN',
+  title: '知屋',
+  description: '一个房屋管理系统',
+  //   使用默认主题
+  //   theme: defaultTheme({
+  //     // 在这里进行配置
+  //   }),
+
+  //   使用hope主题
+  theme: theme,
+  plugins: [
+    searchPlugin({
+      // 配置项
+    }),
+  ],
 };
