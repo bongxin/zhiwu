8908530c43b3fd42d0bce832b0b1925d16b31d65
xiekx <xiekx@huaweisoft.com>
2023-08-14 11:50:21 +0800

    按路由动态显示侧边栏

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,21 +1,44 @@
-import { sidebar } from "vuepress-theme-hope";
+import { sidebar } from 'vuepress-theme-hope';
 
 export default sidebar({
-  "/": [
-    "",
+  '/demo/': [
     {
-      text: "案例",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
+      text: '案例',
+      icon: 'laptop-code',
+      // prefix: '/',
+      link: 'demo/',
+      children: 'structure',
     },
     {
-      text: "文档",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
+      text: '家谱',
+      icon: 'book',
+      // prefix: '/',
+      link: 'clan/',
+      children: 'structure',
     },
-    "slides",
   ],
+  '/guide/': [
+    {
+      text: '文档',
+      icon: 'book',
+      prefix: 'guide/',
+      children: 'structure',
+    },
+    {
+      text: '家谱',
+      icon: 'book',
+      prefix: 'clan/',
+      children: 'structure',
+    },
+    'slides',
+  ],
+  '/clan/': [
+    {
+      text: '家谱',
+      icon: 'book',
+      prefix: 'clan/',
+      children: 'structure',
+    },
+  ],
+  '/': [''],
 });
